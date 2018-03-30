eff7be3d58a80f07d4e51d05cca8c9122d5e66f4
xinghui <caoxinghui58@163.com>
2018-03-30 17:13:18 +0800

    show blog

--- src/components/ShowBlog.vue
+++ src/components/ShowBlog.vue
@@ -27,7 +27,7 @@ export default {
   created(){
       this.$http.get('../../static/posts.json')
             .then(function(data){
-                // console.log(data);
+                console.log(data);
                 this.blogs=data.body.slice(0,10);
                 // console.log(this.blogs);
             })
